bac1c202348008d8e7a42c711859900096c8d128
Nandish BS <nandishbs11@gmail.com>
2024-11-24 13:02:16 +0530

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import 'dotenv/config'
-
+import cors from 'cors'
+import bodyParser from 'body-parser'
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
+app.use(bodyParser.urlencoded())
 app.get('/',(req,res)=>{
     res.send('this is home page')
 })
@@ -25,6 +28,10 @@ app.get('/github',(req,res)=>{
 
 app.get('/anime',(req,res)=>{
     res.send('iam an anime fan')
+})
+
+app.post('/post',(req,res)=>{
+    res.send({'msg' : 'your post request is recieved', yourrequest : req.body})
 })
 
 app.listen(port,()=>{
