cf32ed68a3b7531fc65f5fd974ecb9cd914dae48
Nandish BS <nandishbs11@gmail.com>
2024-11-24 17:16:50 +0530

    as

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use((req, res, next) => {
     );
     next();
   });
-app.use(cors())
+app.use(cors({
+    origin:'*'
+}))
 
 app.get('/',(req,res)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
